95eb22ab3ca32b2f68388535cb26065343b60345
hope.hy.cn <hope.hy.cn@gmail.com>
2019-01-19 02:58:59 +0800

    feat:项目搭建完成 1.使用脚手架初始化项目，其中react-scripts为1.1.5 2.less配置 3.配置antd 4.添加页面路由

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+
+import {
+  Login,
+  NotFound
+} from './pages'
+
+import App from './App';
+
+// import './index.less'
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path="/admin" component={App}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/404" component={NotFound}/>
+      <Redirect to="admin" from="/" exact/>
+      <Redirect to="/404"/>
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
